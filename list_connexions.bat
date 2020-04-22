d5b46d996c247fba7c0c291d4f943afbf272e818
Processus Thief <13833925+Processus-Thief@users.noreply.github.com>
2020-04-22 15:49:59 +0200

    Add files via upload

---
+++ list_connexions.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Chargement...
+powershell -command "$logs = get-eventlog system -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-10);$res = @(); ForEach ($log in $logs) {if($log.instanceid -eq 7001) {$type = \"Logon\"} Elseif ($log.instanceid -eq 7002){$type=\"Logoff\"} Else {Continue} $res += New-Object PSObject -Property @{Time = $log.TimeWritten; \"Event\" = $type; User = (New-Object System.Security.Principal.SecurityIdentifier $Log.ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])}};echo $res > C:\Windows\output.txt; notepad C:\Windows\output.txt;"
